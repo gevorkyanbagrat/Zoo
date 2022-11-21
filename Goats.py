0b32ecb1c8740a3fa526847a1f0504ca56d85f16
Bagrat <geworkyan022005@gmail.com>
2022-11-22 00:31:24 +0300

    goats

---
+++ Goats.py
@@ -0,0 +1,21 @@
+class Goat:
+
+    byome='plain'
+    area=1.5
+    food='трава'
+    predator=False
+    sound='Noone can stop me I feel like the greatest'
+
+
+    def __init__(self, name, amountOfFood, age):
+        self.Name=name
+        self.Amount=amountOfFood
+        self.Age=age
+
+
+    def DoSound(self, number):
+        for i in range(number):
+            print(self.Name, self.sound)
+
+    def Eat(self, Food):
+        Food-=self.amountOfFood
---
+++ main.py
@@ -0,0 +1,3 @@
+from Goats import *
+g1=Goat("Русик", 1, 4)
+g1.DoSound(5)
