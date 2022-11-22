348bc9dd6c57fdccee60d428e1782b6e1ed86329
gevorkyanbagrat <118271511+gevorkyanbagrat@users.noreply.github.com>
2022-11-22 16:54:30 +0300

    update Goats 2

--- Goat.py
+++ Goat.py
@@ -4,11 +4,11 @@ class Goat:
 
     def __init__(self, name, amountOfFood, age):
         self.__Name=name
-        self.__Amount=amountOfFood
+        self.__AmountOfFood=amountOfFood
         self.__Age=age
         self.__byome='plain'
         self.__area=10
-        self.__food='grass'
+        self.__food=[grass']
         self.__predator=False
         self.__sound='meeeee'
         
@@ -20,8 +20,41 @@ class Goat:
     def Eat(self, Food):
         Food-=self.amountOfFood
         
-     @property
-    def SetName:
+    @property
+    def Name(self):
+        return self.__Name
+    @Name.setter
+    def Name(self,name):
+        self.__Name=name
+        
+    @property
+    def Age(self):
+        return self.__Age
+    @Age.setter
+    def Age(self, value):
+        self.__Age=value
+        
+    @property
+    def Byome(self):
+        return self.__Byome
+    
+    @property
+    def Area(self):
+        return self.__area
+    
+    @property
+    def Food(self):
+        return self.__Food
+    @Food.setter
+    def Food(self, unitOfFood):
+        self.__Food.append(unitOfFood)
+                     
+                 
+    
+    
+        
+      
+        
         
     
     
