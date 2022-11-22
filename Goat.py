a9f76f2440bd08f2074eaecae15e34838dbc4acd
gevorkyanbagrat <118271511+gevorkyanbagrat@users.noreply.github.com>
2022-11-22 16:37:19 +0300

    Update Goat.py

--- Goat.py
+++ Goat.py
@@ -1,21 +1,27 @@
 class Goat:
-
-    byome='plain'
-    area=10
-    food='grass'
-    predator=False
-    sound='Noone can stop me I feel like the greatest'
-
+    
+   
 
     def __init__(self, name, amountOfFood, age):
-        self.Name=name
-        self.Amount=amountOfFood
-        self.Age=age
-
-
+        self.__Name=name
+        self.__Amount=amountOfFood
+        self.__Age=age
+        self.__byome='plain'
+        self.__area=10
+        self.__food='grass'
+        self.__predator=False
+        self.__sound='meeeee'
+        
+ 
     def DoSound(self, number):
         for i in range(number):
             print(self.Name, self.sound)
 
     def Eat(self, Food):
         Food-=self.amountOfFood
+        
+     @property
+    def SetName:
+        
+    
+    
