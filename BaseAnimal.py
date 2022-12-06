833ee91b62ef824bda64734bf7f252bab6cd5f0d
Bagrat Gevorkyan <geworkyan022005@gmail.com>
2022-12-06 22:02:48 +0300

    BaseAnimal 2

--- BaseAnimal.py
+++ BaseAnimal.py
@@ -1,11 +1,90 @@
-from BaseAnimal import *
+class BaseAnimal:
+    def __init__(self, type,  name, age):
+        self.__Byome = None
+        self.__type = type
+        self.__Name = name
+        self.__AmountOfFood = 0
+        self.__Age = age
+        self.__area = 0
+        self.__Food = []
+        self.__predator = False
+        self.__sound = ""
+        self.__foodCount = 0
+        self.IsFeeded = False
+
+    def DoSound(self, number):
+        for i in range(number):
+            print(self.Name, self.__sound)
+
+    def Eat(self, Food):
+        if Food in self.__Food:
+            if self.__foodCount > self.__AmountOfFood:
+                print(self.__Name, "я наелся и больше не буду")
+            else:
+                self.__foodCount += 1
+            if self.__foodCount == self.__AmountOfFood:
+                self.IsFeeded = True
+                print(self.__Name,"Я наелся")
+            elif self.__foodCount > self.__AmountOfFood:
+                print(self.__Name,"я наелся и больше не буду")
+        else:
+            print(self.__Name,'Я не ем такое')
 
 
-class Goat(BaseAnimal):
+    @property
+    def Name(self):
+        return self.__Name
 
-    def __init__(self, name, age):
-        super().__init__(Goat, name, age)
-        self.Byome = "plain"
-        self.Sound = "me me me"
-        self.Area = 15
+    @Name.setter
+    def Name(self, name):
+        self.__Name = name
+
+    @property
+    def Age(self):
+        return self.__Age
+
+    @Age.setter
+    def Age(self, value):
+        self.__Age = value
+    @property
+    def Sound(self):
+        return self.__sound
+
+    @Sound.setter
+    def Sound(self, sound):
+        self.__sound=sound
+    @property
+    def Byome(self):
+        return self.__Byome
+
+    @Byome.setter
+    def Byome(self, byome):
+        self.__Byome = byome
+
+    @property
+    def Area(self):
+        return self.__area
+    @Area.setter
+    def Area(self, area):
+        self.__area=area
+
+    @property
+    def Food(self):
+        return self.__Food
+
+    @Food.setter
+    def Food(self, unitOfFood):
+        self.__Food.append(unitOfFood)
+
+    @property
+    def type(self):
+        return self.__type
+
+    @property
+    def amountOfFood(self):
+        return self.__AmountOfFood
+
+    @amountOfFood.setter
+    def amountOfFood(self, amountOfFood):
+        self.__AmountOfFood=amountOfFood
 
--- main.py
+++ main.py
@@ -1,36 +1,14 @@
 from Goat import *
 from Monkey import *
 from Kangaroo import *
-goat1=Goat("Русик", amountOfFood=3, age=4)
-goat1.DoSound(5)
-print(goat1.Name)
-goat1.Name= 'edit'
-print(goat1.Name)
-print(goat1.Age)
-goat1.Age=8
-print(goat1.Age)
-goat1.Food= 'яблоко'
-print(goat1.Food)
-goat1.Eat('яблоко')
-print(goat1.IsFeeded)
-goat1.Eat('як')
-print(goat1.IsFeeded)
+g=Goat("Чик", 4)
+g.DoSound(5)
+print(g.Name, g.Age, g.Food, g.Byome, g.Area, g.type)
+print(g.amountOfFood, g.IsFeeded)
+g.Eat("трава")
+g.Eat("трава")
+g.Eat("трава")
+g.Eat("рога")
 
 
-monkey1 = Monkey("Кура", amountOfFood=2, age=2)
-print(monkey1.Name)
-monkey1.Eat('grape')
-monkey1.Eat('grape')
-monkey1.Eat('grape')
-monkey1.Eat('meat')
-print(monkey1.IsFeeded)
-
-kangaroo1=Kangaroo("Рамзес", amountOfFood=4, age=1)
-print(kangaroo1.Name)
-kangaroo1.DoSound(2)
-kangaroo1.Eat('oats')
-kangaroo1.Eat('oats')
-kangaroo1.Eat('oats')
-kangaroo1.Eat('oats')
-print(kangaroo1.IsFeeded)
 
