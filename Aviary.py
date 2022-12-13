c049f77e09b9eb12ee35dcbd6d3d55ca627daf7a
gevorkyanbagrat <118271511+gevorkyanbagrat@users.noreply.github.com>
2022-12-13 16:33:42 +0300

    Aviary update 3

--- Aviary.py
+++ Aviary.py
@@ -8,18 +8,19 @@ class Aviary:
 
     def AddAnimal(self, animal):
         if not(self.__area >= animal.Area and self.__biome in animal.Biome):
-            print("этот зверь не подходит к такому вольеру1")
-            return
+            print("этот зверь не подходит к такому вольеру по площади или биому")
 
-        if len(self.__listOfAnimals) > 0:
-            if not(self.__listOfAnimals[0].predator == False and animal.predator == False):
-                print("этот зверь не подходит к такому вольеру2")
-                return
-                if not (self.__listOfAnimals[0].type == animal.type):
-                    print("этот зверь не подходит к такому вольеру3")
+        elif len(self.__listOfAnimals) > 0:
+            for i in self.__listOfAnimals:
+                if self.__listOfAnimals[i].IsPredator==True and animal.IsPredator==True and self.__listOfAnimals[i].type != animal.type:
+                    print("этот зверь не подходит к такому вольеру, т.к. хощники разного вида")
+                else:
+                    self.__listOfAnimals.append(animal)
+                    animal.inAviary = self
                     return
-        self.__listOfAnimals.append(animal)
-        animal.inAviary = self
+        else:   
+            self.__listOfAnimals.append(animal)
+            animal.inAviary = self
 
     def DeleteAnimal(self, animal):
         if animal in self.__listOfAnimals:
